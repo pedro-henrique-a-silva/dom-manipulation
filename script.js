a29786d9c7ebc50dcd36916b90c542fc6241f66c
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-11 15:18:26 -0300

    Requisito 1 concluido, criação função mudaTagP

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Seu código aqui
+
+// Requisito 1
+
+const mudaTagP = () => {
+  const tagP = document.querySelector('.center-content p:nth-of-type(2)');
+  tagP.innerHTML = 'Daqui a 2 anos me vejo como um, Dev Casca grossa :)';
+};
+
+mudaTagP()
