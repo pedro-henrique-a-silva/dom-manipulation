afe8b3ae108d0de572028fe9fb8314958f7e5721
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-11 15:31:59 -0300

    Requisito 3 concluido, criação da função mudaFundoCentro

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const mudaTagP = () => {
   const tagP = document.querySelector('.center-content p:nth-of-type(2)');
-  tagP.innerHTML = 'Daqui a 2 anos me vejo como um, Dev Casca grossa :)';
+  tagP.innerHTML = 'Daqui a 2 anos me vejo como um Dev Casca grossa :)';
 };
 
 // Requisito 2
@@ -14,6 +14,13 @@ const mudaFundoMain = () => {
   tagMain.style.backgroundColor = 'rgb(76, 164, 109)';
 };
 
+// Requisito 3
+
+const mudaFundoCentro = () => {
+  const sectionCenter = document.querySelector('.center-content');
+  sectionCenter.style.backgroundColor = 'white';
+};
+
 mudaTagP();
 mudaFundoMain();
-
+mudaFundoCentro();
